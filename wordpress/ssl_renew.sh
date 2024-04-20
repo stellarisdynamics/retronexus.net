6416504c01292fc5b59ae4293a0b0f5721466d3e
retronexus <sebastian.scharf@stellarisdynamics.com>
2024-04-20 15:06:19 +0200

    Update

--- wordpress/ssl_renew.sh
+++ wordpress/ssl_renew.sh
@@ -3,7 +3,7 @@
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
-cd /home/sammy/wordpress/
+cd /home/retronexus/wordpress/
 # $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
